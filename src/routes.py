229db663befe0ce3d084dd6e6fac1a562caa3e62
Angelo <angelo1236540@gmail.com>
2024-04-13 22:13:28 -0500

    v.1.0, routes
    Validacion de usuario desde la DB usando POST para valiadar si el usario esta registrado desde la tabla empleado.
    
    Posdata: toda la tarde haciendo esto jajajaj

--- src/routes.py
+++ src/routes.py
@@ -1,4 +1,4 @@
-from __init__ import app, get_db
+from __init__ import app, get_db, mysql
 from flask import render_template, request
 
 #Endpont pagina principal
@@ -7,18 +7,32 @@ def login():
     return render_template('login.html')
 
 #Valida login de usuario
-@app.route('/validar-login', methods=['POST'])
+@app.route('/validar-login', methods=['GET', 'POST'])
 def validar_login():
     if get_db() is None:
         return "Error en la base de datos"
-    else:
-        # Obtener los datos de un campo del formulario
+
+    if request.method == 'POST' and 'cedula_user' in request.form and 'password_user' in request.form:
         usuario = request.form['cedula_user'] 
-        clave = request.form['password_user']       
-         
-        print(request.form['cedula_user'])
-        print(request.form['password_user'])
+        clave = request.form['password_user']     
+
+        print(usuario)
+        print(clave)
+
+        cur = mysql.connection.cursor()
+        cur.execute('SELECT * FROM empleados WHERE cedula = %s AND contraseña = %s', (usuario, clave))
+        account = cur.fetchone()
+
+        if account:
+            return render_template('home.html')
+        else:
+            return render_template('login.html')
+
+    else:
+        return render_template('login.html')
+
+
         
 @app.route('/home')
 def home():
-    return "Welcome to the Home Page"
+    return render_template('home.html')
